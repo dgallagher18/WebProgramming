dc7a877cc2ac358f63d10f060349fb6fd7cdf885
dgallagher18 <31863581+dgallagher18@users.noreply.github.com>
2017-11-20 17:48:09 -0500

    Add files via upload

---
+++ HOMEWORK/JSmultiplication/grid/script.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = 7;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = 7;
+document.getElementById("result").innerHTML = num1 / num2;
+}
